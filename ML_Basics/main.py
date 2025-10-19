7c76e6e492277eed51dc4dcb8b271b803116ab22
Proto <Humayoon04syed@gmail.com>
2025-10-19 15:58:22 +0500

    numpy multi-dimensional array

--- ML_Basics/main.py
+++ ML_Basics/main.py
@@ -7,8 +7,26 @@ my_list = my_list * 2
 print(my_list) # output [1,2,3,4,1,2,3,4]
 
 # a numpy array is superior tp python python list because it's faster
-array = np.array([1,2,3,4])
-array = array * 2
+array = np.array([1,2,3,4]) * 2
+# array = array * 2
 print(array)
 print(type(array)) # output ndarray -> n-dimensional array
 
+# 0,1,2 and 3 Dimensional array
+arrayx = np.array('A') # output 0
+
+arrayY = np.array(['A', 'B', 'C']) # output 1
+
+arrayZ = np.array([
+    ['A', 'B', 'C'],['D', 'E', 'F'],['G', 'H', 'I']
+]) # output 2 because it's a two dimensional array ie Rows and Column
+
+array3 = np.array([
+    [['A', 'B', 'C'],['D', 'E', 'F'],['G', 'H', 'I']],
+    [['J', 'K', 'L'],['M', 'N', 'O'],['P', 'Q', 'R']],
+    [['S', 'T', 'U'],['V', 'W', 'X'],['Y', 'Z', ' ']]
+    
+]) # output 3 because it's a three dimensional array ie Rows and Column
+
+
+print(array3.ndim)
