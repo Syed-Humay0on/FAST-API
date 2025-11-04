f4611de0ddbcea5a9689046b26a2e964b153c6a0
Proto <Humayoon04syed@gmail.com>
2025-11-04 19:09:46 +0500

    dictionaries series

--- Jupyter/test_pandas.py
+++ Jupyter/test_pandas.py
@@ -3,9 +3,16 @@ import pandas as pd
 # print(pd.__version__)
 
 #Series:
-data = [100.2, 200.2, 400, 401]
+# data = [100.2, 200.2, 400, 401]
+#
+# series = pd.Series(data, index=["a", "b", "c", "d"])
+# series.loc["d"] = 402.76
+#
+# print(series[series > 200.3])
 
-series = pd.Series(data, index=["a", "b", "c", "d"])
-series.loc["d"] = 402.76
+#dictonaries = key:value pairs
+calories = {"day 1": 1750, "day 2": 2100, "day 3": 1700}
 
+series = pd.Series(calories)
+series.loc["day 3"] += 300
 print(series)
